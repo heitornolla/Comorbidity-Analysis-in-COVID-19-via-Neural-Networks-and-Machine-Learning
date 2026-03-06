1f2e9c76b0f9519113743080418e34d03ffc300d
heitor <heitor.nolla@gmail.com>
2026-03-06 13:45:04 -0300

    black linting

--- train.py
+++ train.py
@@ -10,13 +10,15 @@ from models.mlp_2layer import TwoLayerMLP
 from models.mlp_1layer import OneLayerMLP
 
 
-def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader) -> nn.Module:
-    optimizer = optim.Adam(model.parameters(), lr=1e-2)
-    criterion = nn.BCELoss()
+def train(
+    model: nn.Module, train_loader: DataLoader, val_loader: DataLoader
+) -> nn.Module:
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    criterion = nn.BCEWithLogitsLoss()
 
     epochs = 200
     patience = 50
-    best_val_loss = float('inf')
+    best_val_loss = float("inf")
     counter = 0
 
     for epoch in range(epochs):
@@ -27,16 +29,16 @@ def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader) ->
             optimizer.zero_grad()
             out = model(batch_X)
             loss = criterion(out, batch_y)
-            
+
             loss.backward()
             optimizer.step()
 
             train_loss += loss.item()
-        
+
         model.eval()
         val_loss = 0
         correct = 0
-        tp, fn = 0, 0 # Recall
+        tp, fn = 0, 0  # Recall
 
         with torch.no_grad():
             for batch_X, batch_y in val_loader:
@@ -57,22 +59,26 @@ def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader) ->
 
         if avg_val_loss < best_val_loss:
             best_val_loss = avg_val_loss
-            torch.save(model.state_dict(), f'ckpt/{str(model)}_best_val.pth')
+            torch.save(model.state_dict(), f"ckpt/{str(model)}_best_val.pth")
             counter = 0
         else:
             counter += 1
             if counter >= patience:
-                print('Early stopping at epoch ', epoch)
+                print("Early stopping at epoch ", epoch)
                 break
 
         if epoch % 20 == 0:
-            print(f'Epoch {epoch}: Val Loss = {avg_val_loss:.4f} | Acc = {acc:.4f} | Recall = {recall:.4f}')
-        
-    model.load_state_dict(torch.load(f'ckpt/{str(model)}_best_val.pth', weights_only=False))
+            print(
+                f"Epoch {epoch}: Val Loss = {avg_val_loss:.4f} | Acc = {acc:.4f} | Recall = {recall:.4f}"
+            )
+
+    model.load_state_dict(
+        torch.load(f"ckpt/{str(model)}_best_val.pth", weights_only=False)
+    )
     return model
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     train_df, val_df, test_df = get_dfs()
     train_loader, val_loader, test_loader = prepare_loaders(train_df, val_df, test_df)
 
@@ -81,4 +87,6 @@ if __name__ == '__main__':
         trained_model = train(model(), train_loader, val_loader)
 
         targets, preds = get_pytorch_preds(trained_model, test_loader)
-        evaluate_model(targets, preds, model_name=str(model()), log_file=f'logs/{str(model())}.csv')
+        evaluate_model(
+            targets, preds, model_name=str(model()), log_file=f"logs/{str(model())}.csv"
+        )
